c10badbf456bf97cf8f0e1f9d16c90b596cb86fd
JulianLeon <julian.sagberger@gmail.com>
2025-10-13 20:15:41 +0200

    Add Brevo

--- base/views.py
+++ base/views.py
@@ -14,44 +14,6 @@ import logging
 
 logger = logging.getLogger('django')
 
-# Create your views here.
-# def home(request):
-
-#     form_submitted = False
-#     if request.method == 'POST':
-#         form = ContactForm(request.POST)
-#         if form.is_valid():
-#             ## Daten aus Formular holen
-#             name = form.cleaned_data['name']
-#             email = form.cleaned_data['email']
-#             betreff = form.cleaned_data['betreff']
-#             nachricht = form.cleaned_data['nachricht']
-#             form_submitted = True
-#             # form = None
-
-#             full_message = f"Von: {name}\nE-Mail: {email}\n\nNachricht:\n{nachricht}"
-
-#             #Send the Email
-#             email = EmailMessage(
-#                 subject=f'Kontaktanfrage: {betreff}',
-#                 body=f"Von: {name}\nE-Mail: {email}\n\nNachricht:\n{nachricht}",
-#                 from_email=settings.DEFAULT_FROM_EMAIL,
-#                 to=[settings.ADMIN_EMAIL],
-#                 reply_to=[email], 
-#             )
-#             email.send(fail_silently=False)
-            
-            
-#     else:
-#         form = ContactForm()
-
-#     context = {
-#         'form_submitted': form_submitted,
-#         'form': form
-#     }
-
-#     return render(request, 'base/index.html', context)
-
 
 def home(request):
 
@@ -114,6 +76,47 @@ def impressum(request):
 
 def datenschutz(request):
     return render(request, 'base/datenschutz.html')
+
+# Create your views here.
+# def home(request):
+
+#     form_submitted = False
+#     if request.method == 'POST':
+#         form = ContactForm(request.POST)
+#         if form.is_valid():
+#             ## Daten aus Formular holen
+#             name = form.cleaned_data['name']
+#             email = form.cleaned_data['email']
+#             betreff = form.cleaned_data['betreff']
+#             nachricht = form.cleaned_data['nachricht']
+#             form_submitted = True
+#             # form = None
+
+#             full_message = f"Von: {name}\nE-Mail: {email}\n\nNachricht:\n{nachricht}"
+
+#             #Send the Email
+#             email = EmailMessage(
+#                 subject=f'Kontaktanfrage: {betreff}',
+#                 body=f"Von: {name}\nE-Mail: {email}\n\nNachricht:\n{nachricht}",
+#                 from_email=settings.DEFAULT_FROM_EMAIL,
+#                 to=[settings.ADMIN_EMAIL],
+#                 reply_to=[email], 
+#             )
+#             email.send(fail_silently=False)
+            
+            
+#     else:
+#         form = ContactForm()
+
+#     context = {
+#         'form_submitted': form_submitted,
+#         'form': form
+#     }
+
+#     return render(request, 'base/index.html', context)
+
+
+
 
 
 
